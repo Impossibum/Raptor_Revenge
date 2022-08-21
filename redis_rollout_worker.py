3dae9372a10b3316c4246f8ee8bba7245a6578e4
Impossibum <86384743+Impossibum@users.noreply.github.com>
2022-08-21 15:17:32 -0400

    Update redis_rollout_worker.py
    some bug fixing

--- redis_rollout_worker.py
+++ redis_rollout_worker.py
@@ -24,7 +24,6 @@ from rocket_learn.utils.dynamic_gamemode_setter import DynamicGMSetter
 
 def rollout_uploader(redis, q):
     unsent_rollouts = deque([], maxlen=100)
-    print("thread created")
     while True:
         while q.qsize() != 0:
             unsent_rollouts.append(q.get())
@@ -227,39 +226,38 @@ class RedisRolloutWorker:
 
 
     #@functools.lru_cache(maxsize=8)
-    def _get_latest_model(self, version):
+    def _get_latest_model(self, version, current_model):
         # if version in local database, query from database
         # if not, pull from REDIS and store in disk cache
 
         models = self.sql.execute("SELECT parameters FROM MODELS WHERE id == ?", (version,)).fetchall()
-        # if not self.cache_writer:
-        #     if len(models) == 0:
-
-                # time.sleep(1)
-                # models = self.sql.execute("SELECT parameters FROM MODELS WHERE id == ?", (version,)).fetchall()
-        if len(models) == 0:
-            if not self.cache_writer:
+        if not self.cache_writer:
+            if len(models) == 0:
                 return None
-            bytestream = self.redis.get(MODEL_LATEST)
-            model = _unserialize_model(bytestream)
-
-            self.sql.execute('INSERT INTO MODELS (id, parameters) VALUES (?, ?)', (version, bytestream))
-            self.sql.commit()
-
-            previous_check = self.sql.execute("SELECT parameters FROM MODELS WHERE id == ?", (version + 2,)).fetchall()
-            if len(previous_check) > 0:
-                self.sql.execute(f"DELETE FROM MODELS WHERE id == '{version + 2}'")
-                self.sql.commit()
-                # self.sql.execute(f"DELETE id FROM MODELS WHERE id == '{version + 2}'")
-                # self.sql.commit()
         else:
-            # should only ever be 1 version of parameters
-            assert len(models) <= 1
-            # stored as tuple due to sqlite,
-            assert len(models[0]) == 1
+            if len(models) == 0:
 
-            bytestream = models[0][0]
-            model = _unserialize_model(bytestream)
+                bytestream = self.redis.get(MODEL_LATEST)
+                model = _unserialize_model(bytestream)
+
+                self.sql.execute('INSERT INTO MODELS (id, parameters) VALUES (?, ?)', (version, bytestream))
+                self.sql.commit()
+
+                previous_check = self.sql.execute("SELECT parameters FROM MODELS WHERE id == ?", (version + 2,)).fetchall()
+                if len(previous_check) > 0:
+                    self.sql.execute(f"DELETE FROM MODELS WHERE id == '{version + 2}'")
+                    self.sql.commit()
+                    # self.sql.execute(f"DELETE id FROM MODELS WHERE id == '{version + 2}'")
+                    # self.sql.commit()
+                return model
+
+        # should only ever be 1 version of parameters
+        assert len(models) <= 1
+        # stored as tuple due to sqlite,
+        assert len(models[0]) == 1
+
+        bytestream = models[0][0]
+        model = _unserialize_model(bytestream)
 
         return model
 
@@ -288,7 +286,7 @@ class RedisRolloutWorker:
             # Only try to download latest version when new
             if latest_version != available_version:
                 if self.local_cache_name:
-                    updated_agent = self._get_latest_model(available_version)
+                    updated_agent = self._get_latest_model(available_version, self.current_agent)
                 else:
                     model_bytes = self.redis.get(MODEL_LATEST)
                     if model_bytes is None:
@@ -296,9 +294,9 @@ class RedisRolloutWorker:
                         continue  # This is maybe not necessary? Can't hurt to leave it in.
                     updated_agent = _unserialize_model(model_bytes)
 
-                # if updated_agent is not None:
-                latest_version = available_version
-                self.current_agent = updated_agent
+                if updated_agent is not None:
+                    latest_version = available_version
+                    self.current_agent = updated_agent
 
             n += 1
             pretrained_choice = None
@@ -383,7 +381,7 @@ class RedisRolloutWorker:
                                             self.send_obs, self.send_gamestates, True))
 
                 def send():
-                    self.rollout_q.put(rollout_bytes)
+                    self.rollout_q.put_nowait(rollout_bytes)
 
                 send()
 
